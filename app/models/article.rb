06e05ff8371b29a77d4e45c4e043a2e82ea817e3
rymcmahon <toedragdangle9@gmail.com>
2015-12-15 17:14:44 -0600

    added create and destroy comment functionality

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
 	has_and_belongs_to_many :categories
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	validates :title, presence: true
 	validates :text, presence: true
 end
