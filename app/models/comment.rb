c651fbf6daad86e1ec98f3a30f266445d3485362
rymcmahon <toedragdangle9@gmail.com>
2016-07-23 15:57:02 -0500

    added scope for approved comment query, added retina touch icons, and added dynamic title and meta tags

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,6 @@ class Comment < ActiveRecord::Base
 	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, :on => :create
 	attr_accessor :subtitle
   validates :subtitle, :invisible_captcha => true
+
+  scope :approved, -> { where(approved: true) }
 end
