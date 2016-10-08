82b4c3be58c87d6f8ff7dd88cc375809e04c81d5
rymcmahon <toedragdangle9@gmail.com>
2016-10-08 08:21:19 -0500

    fixed indentation on article.rb and article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,69 +1,68 @@
 class ArticlesController < ApplicationController
-	before_action :admin_user, only: [:edit, :update, :new, :create, :destroy]
+  before_action :admin_user, only: [:edit, :update, :new, :create, :destroy]
 
-	def index
-		@articles = Article.all
-		@categories = Category.all
-	end
+  def index
+    @articles = Article.all
+    @categories = Category.all
+  end
 
-	def show
-		@article = Article.find(params[:id])
-		@categories = Category.all
-		@comments = @article.comments.all
-		@article_comments = @article.comments
-	end
+  def show
+    @article = Article.find(params[:id])
+    @categories = Category.all
+    @comments = @article.comments.all
+    @article_comments = @article.comments
+  end
 
-	def new
-		@article = Article.new
-		@categories = Category.all
-	end
+  def new
+    @article = Article.new
+    @categories = Category.all
+  end
 
-	def edit
-		@article = Article.find(params[:id])
-		@categories = Category.all
-	end
+  def edit
+    @article = Article.find(params[:id])
+    @categories = Category.all
+  end
 
-	def create
-		@article = Article.new(article_params)
-		@categories = Category.all
+  def create
+    @article = Article.new(article_params)
+    @categories = Category.all
 
-		if @article.save
-			redirect_to @article
-		else
-			render 'new'
-		end
-	end
-
-	def update
-		params[:article][:category_ids] ||= []
-
-		@article = Article.find(params[:id])
-
-		if @article.update(article_params)
-			redirect_to @article
-		else
-			render 'edit'
-		end
-	end
-
-	def destroy
-		@article = Article.find(params[:id])
-		@article.destroy
-
-		redirect_to articles_path
-	end
-
-	private
-
-		def article_params
-			params.require(:article).permit(:title, :text, :category_ids => [])
-		end
-
-    def admin_user
-    	unless current_user.try(:admin?)
-        flash[:danger] = "You ain't no Admin, fool!"
-      	redirect_to(root_url)
-      end
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
     end
+  end
 
+  def update
+    params[:article][:category_ids] ||= []
+
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text, :category_ids => [])
+  end
+
+  def admin_user
+    unless current_user.try(:admin?)
+      flash[:danger] = "You ain't no Admin, fool!"
+      redirect_to(root_url)
+    end
+  end
 end
