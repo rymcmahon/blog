e4e04656b89e6a32389e918a0a3a12f1f2ffddf2
rymcmahon <toedragdangle9@gmail.com>
2018-09-29 19:26:10 -0400

    added host redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :redirect_if_old
+
+  protected
+
+  def redirect_if_old
+    if request.host == 'rymcmahon.com'
+      redirect_to "#{request.protocol}ryanmcmahon.org#{request.fullpath}", :status => :moved_permanently
+    end
+  end
 end
