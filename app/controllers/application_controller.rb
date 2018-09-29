668875e1b171e83869bf38f86c98cec3d6b1a748
rymcmahon <toedragdangle9@gmail.com>
2018-09-29 19:32:10 -0400

    switch to before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
 
-  before_action :redirect_if_old
+  before_filter :redirect_if_old
 
   protected
 
