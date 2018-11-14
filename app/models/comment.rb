64fe2a026b308c17e0cb1c732ae1ad475a6e1aee
rymcmahon <toedragdangle9@gmail.com>
2018-11-13 19:50:28 -0500

    stop russia

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :article
   validates :name, presence: true, :on => :create
 	validates :body, presence: true, :on => :create
+  validates :body, format: { with: /\A[a-zA-Z0-9]*\z/, message: "I only speak English" }
 	validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, :on => :create
 	attr_accessor :subtitle
   validates :subtitle, :invisible_captcha => true
